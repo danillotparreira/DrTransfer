8df0ca08684e9299fea964246e8290b7892c2770
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-05-30 18:34:11 -0300

    Foi criado a classe médico

---
+++ src/main/java/br/ufg/inf/drtransfer/model/Medico.java
@@ -0,0 +1,19 @@
+package br.ufg.inf.drtransfer.model;
+
+public class Medico {
+
+    private String nome;
+
+    public Medico(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
