23e4ea2599c1407644ff8c1e68fd4df3a52bb8c4
ommathias <mathias.ufg@gmail.com>
2024-05-30 20:51:32 -0300

    EnumSexo+Paciente

--- src/main/java/br/ufg/inf/drtransfer/model/Paciente.java
+++ src/main/java/br/ufg/inf/drtransfer/model/Paciente.java
@@ -1,11 +1,13 @@
 package br.ufg.inf.drtransfer.model;
 
 import br.ufg.inf.drtransfer.generic.model.SuperClasse;
+import br.ufg.inf.drtransfer.model.enums.Sexo;
 
 public class Paciente extends SuperClasse {
 
     private String name;
-    private String sexo;
+
+    private Sexo sexo;
 
     private String cpf;
 
@@ -23,6 +25,14 @@ public class Paciente extends SuperClasse {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
     }
 
 }
