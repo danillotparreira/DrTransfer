2b8af01aed6be4e496e3025d09482dff081c73c4
ommathias <mathias.ufg@gmail.com>
2024-05-30 20:20:40 -0300

    GetterSetter

--- src/main/java/br/ufg/inf/drtransfer/model/Paciente.java
+++ src/main/java/br/ufg/inf/drtransfer/model/Paciente.java
@@ -5,4 +5,21 @@ import br.ufg.inf.drtransfer.generic.model.SuperClasse;
 public class Paciente extends SuperClasse {
 
     private String name;
+    private String cpf;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
